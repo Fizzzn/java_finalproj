ace55bebe9b9f5f19a6f341ae9d7a44fd8bc85c3
Fizzzn <btordz6@gmail.com>
2025-04-30 14:08:10 -0500

    Update deck.java

--- src/blackjack/deck.java
+++ src/blackjack/deck.java
@@ -1,10 +1,10 @@
 package blackjack;
 import java.util.*;
 
-public class deck {
-    private List<card> cards = new ArrayList<>();
+public class Deck {
+    private List<Card> cards = new ArrayList<>();
 
-    public deck() {
+    public Deck() {
         String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
         String[] symbols = {"H", "D", "C", "S"}; // String[] symbols = {"♥", "♦", "♣", "♠"};
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -12,13 +12,13 @@ public class deck {
         for (int i = 0; i < suits.length; i++) {
             String symbol = symbols[i];
             for (String rank : ranks) {
-                cards.add(new card(rank, symbol)); 
+                cards.add(new Card(rank, symbol)); 
             }
         }
         Collections.shuffle(cards);
     }
 
-    public card draw() {
+    public Card draw() {
         return cards.remove(0);
     }
 }
